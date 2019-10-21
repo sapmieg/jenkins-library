06f05c08e3c51514c3def2aa3fd055c89380579c
Daniel Mieg <daniel.mieg@sap.com>
2019-10-21 13:55:12 +0200

    dev

--- vars/pullSoftwareComponentToAbapCloudPlatform.groovy
+++ vars/pullSoftwareComponentToAbapCloudPlatform.groovy
@@ -104,9 +104,7 @@ private Map triggerPull(Map configuration, String url, String authToken) {
         -H 'Content-Type: application/json' \
         -H 'x-csrf-token: ${xCsrfToken}' \
         --cookie cookieJar.txt \
-        -d '{ 
-            "sc_name":"Z_DEMO_DM" 
-        }'
+        -d '{\n"sc_name":"Z_DEMO_DM"\n}'
     """
     // | grep -E 'x-csrf-token|set-cookie' tokenAndCookie.txt
 
