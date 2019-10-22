b2a3d0229c3bcd5fd8e371e747a825e461fd9a30
Daniel Mieg <daniel.mieg@sap.com>
2019-10-22 11:30:28 +0200

    dev

--- vars/pullSoftwareComponentToAbapCloudPlatform.groovy
+++ vars/pullSoftwareComponentToAbapCloudPlatform.groovy
@@ -107,7 +107,8 @@ private String triggerPull(Map configuration, String url, String authToken) {
     echo responseJson.d.status
     if (responseJson.d.status == "R") {
         echo responseJson.d.status_descr
-        echo responseJson.d."__metadata"
+        echo.responseJson.d.uuid
+        echo responseJson.d.
         uri = responseJson.d."__metadata".uri
     }
 
