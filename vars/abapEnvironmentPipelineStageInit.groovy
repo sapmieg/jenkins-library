3f9ba8cf61ac82efd4cd6f6c1f3fc6d1454c7373
Daniel Mieg <daniel.mieg@sap.com>
2020-06-25 11:15:28 +0200

    Test2

--- vars/abapEnvironmentPipelineStageInit.groovy
+++ vars/abapEnvironmentPipelineStageInit.groovy
@@ -20,6 +20,26 @@ void call(Map parameters = [:]) {
     piperStageWrapper (script: script, stageName: stageName, stashContent: [], stageLocking: false, ordinal: 1, telemetryDisabled: true) {
 
         checkout scm
+        sh '''
+        [ -d "jenkins-library" ] && rm -r jenkins-library
+        git clone https://github.com/DanielMieg/jenkins-library
+        cd jenkins-library
+        git checkout abapPipeline
+        git log -3
+        '''
+
+        dockerExecute(
+            script: script,
+            dockerImage: 'golang',
+            dockerEnvVars: [GOPATH: '/jenkinsdata/abapPipeline Test/workspace']
+        ) {
+            sh '''
+                cd jenkins-library
+                go build -o piper .
+                chmod +x piper
+                cp piper ..
+            '''
+        }
         setupCommonPipelineEnvironment script: script, customDefaults: parameters.customDefaults
 
         // load default & individual configuration
