3a4438454cf074ff426ba60983c408843a4eb1c2
Daniel Mieg <daniel.mieg@sap.com>
2020-06-25 16:11:19 +0200

    Test23

--- vars/abapEnvironmentPipelineStageInit.groovy
+++ vars/abapEnvironmentPipelineStageInit.groovy
@@ -40,7 +40,7 @@ void call(Map parameters = [:]) {
 
         //Config of Addon Pipeline
 
-        String[] repositories
+        def repositories = []
         repositories.add('Z_DEMO_DM_BRANCH')
         script.commonPipelineEnvironment.setAddonRepositoryNames(repositories)
         script.commonPipelineEnvironment.writeToDisk(script)
