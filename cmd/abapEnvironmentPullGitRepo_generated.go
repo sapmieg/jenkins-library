39bd4a0880b2973009c015a5a5f376da1749b18d
Daniel Mieg <daniel.mieg@sap.com>
2020-06-25 10:49:09 +0200

    Test for addon.yml

--- cmd/abapEnvironmentPullGitRepo_generated.go
+++ cmd/abapEnvironmentPullGitRepo_generated.go
@@ -130,7 +130,7 @@ func abapEnvironmentPullGitRepoMetadata() config.StepData {
 					},
 					{
 						Name:        "repositoryNames",
-						ResourceRef: []config.ResourceReference{},
+						ResourceRef: []config.ResourceReference{{Name: "commonPipelineEnvironment", Param: "addonRepositoryNames"}},
 						Scope:       []string{"PARAMETERS", "STAGES", "STEPS"},
 						Type:        "[]string",
 						Mandatory:   true,
