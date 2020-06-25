c6d6a3a94cc9398f695b1856a6658bc905e5868e
Daniel Mieg <daniel.mieg@sap.com>
2020-06-25 13:52:36 +0200

    Test11

--- vars/commonPipelineEnvironment.groovy
+++ vars/commonPipelineEnvironment.groovy
@@ -6,6 +6,8 @@ import groovy.json.JsonOutput
 
 class commonPipelineEnvironment implements Serializable {
 
+    def addonRepositoryNames
+
     //stores version of the artifact which is build during pipeline run
     def artifactVersion
     def originalArtifactVersion
