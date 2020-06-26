a8e811f1530f933788b3bbb556f3a7bf490455bd
Daniel Mieg <daniel.mieg@sap.com>
2020-06-26 15:40:27 +0200

    run generator

--- cmd/abapEnvironmentPullGitRepo_generated.go
+++ cmd/abapEnvironmentPullGitRepo_generated.go
@@ -130,7 +130,7 @@ func abapEnvironmentPullGitRepoMetadata() config.StepData {
 					},
 					{
 						Name:        "repositoryNames",
-						ResourceRef: []config.ResourceReference{{Name: "commonPipelineEnvironment", Param: "addonRepositoryNames"}},
+						ResourceRef: []config.ResourceReference{{Name: "commonPipelineEnvironment", Param: "custom/addonRepositoryNames"}},
 						Scope:       []string{"PARAMETERS", "STAGES", "STEPS"},
 						Type:        "[]string",
 						Mandatory:   true,
