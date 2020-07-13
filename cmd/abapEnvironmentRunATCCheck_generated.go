f20583e6b310f6de2cd4c67fc24d9d4abfb5a68b
Daniel Mieg <daniel.mieg@sap.com>
2020-07-13 15:13:47 +0200

    T12

--- cmd/abapEnvironmentRunATCCheck_generated.go
+++ cmd/abapEnvironmentRunATCCheck_generated.go
@@ -114,7 +114,7 @@ func abapEnvironmentRunATCCheckMetadata() config.StepData {
 				Parameters: []config.StepParameters{
 					{
 						Name:        "atcConfig",
-						ResourceRef: []config.ResourceReference{{Name: "commonPipelineEnvironment", Param: "addonAtcConfig"}},
+						ResourceRef: []config.ResourceReference{{Name: "commonPipelineEnvironment", Param: "custom/addonAtcConfig"}},
 						Scope:       []string{"PARAMETERS", "STAGES", "STEPS"},
 						Type:        "string",
 						Mandatory:   true,
